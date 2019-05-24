4e1f15d4d3238b82cbe95d2c7d814e2fd5502b8c
Sergio <sergiohidalgocaceres@gmail.com>
2019-05-23 20:19:14 -0500

    Rutas hijas

---
+++ Angular/Clase08/appRutasHijas/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListadoUsuariosComponent } from './listado-usuarios/listado-usuarios.component';
+import { NuevoUsuarioComponent } from './nuevo-usuario/nuevo-usuario.component';
+import { EdicionUsuarioComponent } from './edicion-usuario/edicion-usuario.component';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+import { NuevoClienteComponent } from './nuevo-cliente/nuevo-cliente.component';
+import { EdicionClienteComponent } from './edicion-cliente/edicion-cliente.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from "@angular/router"
+
+const rutas: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "usuario", component: ListadoUsuariosComponent, children: [
+      { path: "", component: ListadoUsuariosComponent },
+      { path: "nuevo", component: NuevoUsuarioComponent },
+      { path: "edicion/:id", component: EdicionUsuarioComponent }
+    ]
+  },
+  {
+    path: "cliente", children: [
+      { path: "", component: ListadoClientesComponent },
+      { path: "nuevo", component: NuevoClienteComponent },
+      { path: "edicion/:id", component: EdicionClienteComponent }
+    ]
+  },
+  { path: "**", redirectTo: "" }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoUsuariosComponent,
+    NuevoUsuarioComponent,
+    EdicionUsuarioComponent,
+    ListadoClientesComponent,
+    NuevoClienteComponent,
+    EdicionClienteComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(rutas)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
