0233927aa26fb0a16efc4df70585beea8f1829ed
Sergio <sergiohidalgocaceres@gmail.com>
2019-05-28 20:00:49 -0500

    clase generica

---
+++ Angular/Clase09/appControladorForm/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { AlumnoService } from './alumno.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private alumnoService: AlumnoService) { }
+
+  listar() {
+    console.log("LISTAR", this.alumnoService.listar("nombre"))
+  }
+
+  listarPaginado() {
+    console.log("LISTAR PAGINADO", this.alumnoService.listarPaginado(3, 3, "apellido"))
+  }
+
+  detallar() {
+    console.log("DETALLAR", this.alumnoService.detallar(1))
+  }
+
+  insertar() {
+    this.alumnoService.insertar({ nombre: "Carmen", apellido: "Lara", colegio: "Guadalupe" })
+
+    console.log("INSERTAR", this.alumnoService.listar())
+  }
+
+  modificar() {
+    this.alumnoService.modificar({ nombre: "Juanito", apellido: "Alimaña", colegio: "La Calle" }, 1)
+
+    console.log("MODIFICAR", this.alumnoService.listar())
+  }
+
+  eliminar() {
+    this.alumnoService.eliminar(0)
+
+    console.log("ELIMINAR", this.alumnoService.listar())
+  }
+
+}
